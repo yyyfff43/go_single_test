a370c4522bf8842c8b55f594cd5240d4b6d2b23f
yangfan@zongheng.com <248037973@qq.com>
2021-12-10 10:49:38 +0800

    接口的定义初学，建注释里的实例解释

--- test_oop.go
+++ test_oop.go
@@ -72,3 +72,40 @@ func main (){
 	}
 	 */
 }
+
+/*
+接口(非侵入式)
+在Go语言中，一个类只需要实现了接口要求的所有函数，我们就说这个类实现了该接口，
+例如：
+type File struct {
+ // ...
+}
+func (f *File) Read(buf []byte) (n int, err error){方法内部功能省略……}
+func (f *File) Write(buf []byte) (n int, err error){方法内部功能省略……}
+func (f *File) Seek(off int64, whence int) (pos int64, err error){方法内部功能省略……}
+func (f *File) Close() error
+这里我们定义了一个File类，并实现有Read()、Write()、Seek()、Close()等方法。设
+想我们有如下接口：
+type IFile interface {
+ Read(buf []byte) (n int, err error)
+ Write(buf []byte) (n int, err error)
+ Seek(off int64, whence int) (pos int64, err error)
+ Close() error
+}
+type IReader interface {
+ Read(buf []byte) (n int, err error)
+}
+type IWriter interface {
+ Write(buf []byte) (n int, err error)
+}
+type ICloser interface {
+ Close() error
+}
+尽管File类并没有从这些接口继承，甚至可以不知道这些接口的存在，但是File类实现了
+这些接口，可以进行赋值：
+var file1 IFile = new(File)
+var file2 IReader = new(File)
+var file3 IWriter = new(File)
+var file4 ICloser = new(File)
+(如果有另外的File2类等实现了这些同名接口但不同方法内部的功能，均可以赋值给这些对象，实现接口的多钟实例化)
+ */

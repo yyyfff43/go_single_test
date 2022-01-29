1e6f8548a13e7e7bb7fd45dba1c9ce26808ae7df
yyyfff43 <248037973u@qq.com>
2022-01-29 19:53:11 +0800

    加入数字字面量语法样例

--- test.go
+++ test.go
@@ -4,6 +4,21 @@ import "fmt"
 
 func main() {
 
+	//数字
+	// 十进制
+	var a int = 10
+	fmt.Printf("%d \n", a)  // 10
+	fmt.Printf("%b \n", a)  // 1010  占位符%b表示二进制
+
+	// 八进制  以0开头
+	var b int = 077
+	fmt.Printf("%o \n", b)  // 77
+
+	// 十六进制  以0x开头
+	var c int = 0xff
+	fmt.Printf("%x \n", c)  // ff
+	fmt.Printf("%X \n", c)  // FF
+
 	//字符串遍历
 	str := "Hello,世界"
 	fmt.Println(string(str[0]))
